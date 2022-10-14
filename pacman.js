2d4085b88e7610d596ad46b1ac0fc71697bdde87
afgarcia410 <115699382+afgarcia410@users.noreply.github.com>
2022-10-14 18:10:20 +0200

    Add files via upload

--- pacman.js
+++ pacman.js
@@ -1,20 +1,49 @@
-var mynamespace=mynamespace|| {}
-mynamespace={
-    Game:class{
-        constructor(){
-            this.tablero=tablero;
-        }
-    }
-}
-var tablero=[[0,0,1],
-             [0,1,0],
-             [0,0,0],
-             [0,0,0],
-             [1,0,0]];
-var cogeArray=document.getElementById("campoArray");
-for (let i = 0; i < tablero.length; i++) {
-        const element = tablero[i]; 
-        console.log(element);
-        cogeArray.innerHTML=tablero+"</br>";
+var mynamespace = mynamespace || {};
+
+var tablero = [
+  ["X", 0, 1],
+  [0, 1, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+  [1, 0, 0],
+];
+let vacio = "";
+var cogeArray = document.getElementById("campoArray");
+var textoMov = document.getElementById("texto");
+tablero.forEach(miFuncion);
+cogeArray.innerHTML = vacio;
+
+function miFuncion(valor, index, array) {
+  vacio += valor + "<br>";
 }
 
+moverArriba = function () {
+  //Añadir splice()
+  var arriba = document.getElementById("boton1");
+
+  if (arriba.onclick) {
+    var valor = (textoMov.innerHTML = "arriba");
+    console.log(valor);
+  }
+};
+moverAbajo = function () {
+  var arriba = document.getElementById("boton2");
+  if (arriba.onclick) {
+    var valor = (textoMov.innerHTML = "abajo");
+    console.log(valor);
+  }
+};
+moverDerecha = function () {
+  var arriba = document.getElementById("boton3");
+  if (arriba.onclick) {
+    var valor = (textoMov.innerHTML = "derecha");
+    console.log(valor);
+  }
+};
+moverIzquierda = function () {
+  var arriba = document.getElementById("boton4");
+  if (arriba.onclick) {
+    var valor = (textoMov.innerHTML = "izquierda");
+    console.log(valor);
+  }
+};
